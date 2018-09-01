29be9db2b09507d2ea33c3772203b2fa11eff21c
vinay <krvinay68@gmail.com>
2018-09-01 23:27:49 +0530

    project setup created

---
+++ backendshopping/src/main/java/com/demoshopping/backendshopping/App.java
@@ -0,0 +1,13 @@
+package com.demoshopping.backendshopping;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
